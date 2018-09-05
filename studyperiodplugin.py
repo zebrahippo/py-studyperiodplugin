8a993a11d77e2cebb36cb8fd8748b156e2a1c18c
zebrahippo <42525711+zebrahippo@users.noreply.github.com>
2018-09-05 21:37:38 +0800

    Update studyperiodplugin.py

--- studyperiodplugin.py
+++ studyperiodplugin.py
@@ -12,12 +12,8 @@ def main():
         fireFoxOptions = webdriver.FirefoxOptions()
         browser = webdriver.Firefox(firefox_options=fireFoxOptions)
         browser.maximize_window()
-        
         browser.implicitly_wait(6)
-
         browser.get(url)
-
-        #browser.minimize_window()
         
         #手动输入账户信息，并根据网页当前的验证码输入验证码
         username = raw_input(unicode('用户名：','utf-8').encode('gbk'))
@@ -40,8 +36,7 @@ def main():
 
         #总挂机分钟数
         total_mins = 0
-		
-		    err = 0
+	err = 0
 
         while True:
             reset_mins = random.randint(12,14)
@@ -62,10 +57,10 @@ def main():
                 dialog_box.accept()
                 time.sleep(3)
             except:
-			          err += 1
+		err += 1
             else:
                 total_mins += reset_mins+1
-				        print total_mins
+		print total_mins
 
             #超过240分钟结束循环，因为每天最多有效学时240分钟（4小时）
             if total_mins >= 240 or err >= 2:
